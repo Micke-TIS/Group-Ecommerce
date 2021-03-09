e51be1bef61ca17918cbd48bccb1abb1238748c5
Oliwer Löfgren <oliwerl@outlook.com>
2021-03-09 16:39:45 +0100

    ...

--- includes/header.php
+++ includes/header.php
@@ -33,4 +33,5 @@
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+      <!-- hej hej -->
 </head>
