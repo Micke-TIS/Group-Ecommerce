5c2bfd08265afb54f309f067ae27990e6fa0ce2c
Oliwer Löfgren <oliwerl@outlook.com>
2021-03-10 10:39:25 +0100

    Databas fixad
    (Skriv eller gör ändringar om det är något fel med databasen)

---
+++ includes/config.php
@@ -0,0 +1,20 @@
+<?php
+// Database connection (Oliwer & Kristoffer)
+function OpenCon()
+ {
+ $dbhost = "localhost";
+ $dbuser = "root";
+ $dbpass = "";
+ $db = "Webbservone";
+ $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+
+ return $conn;
+ }
+
+function CloseCon($conn)
+ {
+ $conn -> close();
+ }
+
+ ?>
+ 
