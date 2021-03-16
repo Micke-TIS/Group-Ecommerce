083d28d0f906403980a1546ed68099b3df89c4f9
Juliet TP <juliettanp@gmail.com>
2021-03-16 16:18:21 +0100

    Footer i brand dynamisk

--- brand.php
+++ brand.php
@@ -155,47 +155,11 @@
 
     <!-- end brand -->
 
-    <!-- footer -->
-    <footer>
-        <div id="contact" class="footer">
-            <div class="container">
-                <div class="row pdn-top-30">
-                    <div class="col-md-12 ">
-                        <div class="footer-box">
-                            <div class="headinga">
-                                <h3>Address</h3>
-                                <span>Healing Center, 176 W Streetname,New York, NY 10014, US</span>
-                                <p>(+71) 8522369417
-                                    <br>demo@gmail.com</p>
-                            </div>
-                            <ul class="location_icon">
-                                <li> <a href="#"><i class="fa fa-facebook-f"></i></a></li>
-                                <li> <a href="#"><i class="fa fa-twitter"></i></a></li>
-                                <li> <a href="#"><i class="fa fa-instagram"></i></a></li>
-
-                            </ul>
-                            <div class="menu-bottom">
-                                <ul class="link">
-                                    <li> <a href="#">Home</a></li>
-                                    <li> <a href="#">About</a></li>
-
-                                    <li> <a href="#">Brand </a></li>
-                                    <li> <a href="#">Specials  </a></li>
-                                    <li> <a href="#"> Contact us</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="copyright">
-                <div class="container">
-                    <p>© 2019 All Rights Reserved. Design By<a href="https://html.design/"> Free Html Templates</a></p>
-                </div>
-            </div>
-        </div>
-    </footer>
-    <!-- end footer -->
+    <?php
+    // Some includes for footer (Juliet)
+      include_once("includes/footer.php");
+    ?>
+    
     <!-- Javascript files-->
     <script src="js/jquery.min.js"></script>
     <script src="js/popper.min.js"></script>
