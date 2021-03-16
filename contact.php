9645fcd232a3e9f465a943f9a847b825d9971cf5
donleal <tismma18krilea@innovationsgymnasiet.se>
2021-03-16 16:20:45 +0100

    include kod

--- contact.php
+++ contact.php
@@ -6,9 +6,10 @@
 
 <body class="main-layout">
     <!-- loader  -->
-    <div class="loader_bg">
-        <div class="loader"><img src="images/loading.gif" alt="#" /></div>
-    </div>
+    <?php
+    // Some includes for loader(Kristoffer)
+      include_once("includes/loader.php");
+    ?>
     <!-- end loader -->
     <!-- header -->
     <header>
