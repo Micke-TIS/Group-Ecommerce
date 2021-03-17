ab37ebb6384c64761bcdb0a14cc862324cd4781f
h0nkan <56107612+hnkk@users.noreply.github.com>
2021-03-17 10:38:06 +0100

    -

--- contact.php
+++ contact.php
@@ -18,20 +18,6 @@
       include_once("includes/menu.php");
     ?>
 
-
-    <div class="brand_color">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="titlepage">
-                        <h2>Contact Us</h2>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    </div>
-
     <?php
     // Some includes for header (oliwer)
       include_once("includes/contact.php");
