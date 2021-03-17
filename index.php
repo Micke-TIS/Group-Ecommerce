06c7e686a35ee0913a8621d5e91b3b2045ade65c
Oliwer Löfgren <oliwerl@outlook.com>
2021-03-17 10:56:33 +0100

    about dynamisk.. igen..

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <!-- end loader -->
     <!-- header -->
     <header>
-      
+
         <!-- header inner -->
         <div class="header">
 
@@ -124,28 +124,10 @@
         </div>
     </section>
 
-    <!-- about -->
-    <div class="about">
-        <div class="container">
-            <div class="row">
-                <div class="col-xl-5 col-lg-5 col-md-5 co-sm-l2">
-                    <div class="about_img">
-                        <figure><img src="images/about.png" alt="img" /></figure>
-                    </div>
-                </div>
-                <div class="col-xl-7 col-lg-7 col-md-7 co-sm-l2">
-                    <div class="about_box">
-                        <h3>About Us</h3>
-                        <span>Our Mobile Shop</span>
-                        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of It is a long established fact that a reader will be distracted by the </p>
-
-                    </div>
-
-            </div>
-        </div>
-    </div>
-    </div>
-    <!-- end about -->
+    <?php
+    // Some includes for about (oliwer)
+      include_once("includes/about.php");
+    ?>
 
     <?php
     // Some includes for header (oliwer)
