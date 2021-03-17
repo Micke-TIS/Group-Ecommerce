bec8d6a3ffcaf9f50cf33a28c86a3d693d05d56b
h0nkan <56107612+hnkk@users.noreply.github.com>
2021-03-17 10:40:41 +0100

    Contact.php dynamisk

--- includes/contact.php
+++ includes/contact.php
@@ -1,16 +1,3 @@
-<div class="brand_color">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="titlepage">
-                    <h2>Contact Us</h2>
-                </div>
-            </div>
-        </div>
-    </div>
-
-</div>
-
 <!-- contact -->
 <div class="contact">
     <div class="container">
