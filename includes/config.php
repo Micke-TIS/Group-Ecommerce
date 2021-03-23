a436579e25b84063e8638548fcaeae6573980465
donleal <tismma18krilea@innovationsgymnasiet.se>
2021-03-23 16:02:00 +0100

    .

--- includes/config.php
+++ includes/config.php
@@ -13,6 +13,20 @@ function OpenCon()
 $query = mysqli_query($conn, "SELECT * FROM tbl_articles");
 while($row = mysqli_fetch_assoc($query))
 {
+$menu_about = $row['menu_about'];
+$menu_brand = $row['menu_brand'];
+$menu_specials = $row['menu_specials'];
+$menu_contact = $row['menu_contact'];
+}
+?>
+
+
+<?php
+$query = mysqli_query($conn, "SELECT * FROM tbl_articles");
+while($row = mysqli_fetch_assoc($query))
+{
+
   $title_about = $row['title_about'];
+  $content = $row['content'];
 }
  ?>
