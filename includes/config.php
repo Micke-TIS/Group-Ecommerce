12b631ef0c96cc47fcff63fbddaa02130253803c
Micke <mikael.johanssonj@gmail.om>
2021-03-23 16:56:16 +0100

    Made some stuff so we can reach the database.

--- includes/config.php
+++ includes/config.php
@@ -1,25 +1,24 @@
 <?php
-//Database connection (Kristoffer/Oliwer)
-function OpenCon()
- {
- $dbhost = "localhost";
- $dbuser = "root";
- $dbpass = "";
- $db = "webbservone";
- $conn=mysqli_connect($servername,$username,$password,"$dbname");
- if(!$conn){
-    die('Could not Connect My Sql:' .mysql_error());
- }
-?>
+//Database connection
 
-<?php
+$servername='localhost';
+$username='root';
+$password='';
+$dbname = "webbservone";
+$conn=mysqli_connect($servername,$username,$password,"$dbname");
+if(!$conn){
+   die('Could not Connect My Sql:' .mysql_error());
+}
+
+
+//Select specific columns from DB
 $query = mysqli_query($conn, "SELECT * FROM tbl_articles");
 while($row = mysqli_fetch_assoc($query))
 {
-$menu_about = $row['menu_about'];
-$menu_brand = $row['menu_brand'];
-$menu_specials = $row['menu_specials'];
-$menu_contact = $row['menu_contact'];
-$title_about = $row['title_about'];
+  $menu_about = $row['menu_about'];
+  $menu_brand = $row['menu_brand'];
+  $menu_specials = $row['menu_specials'];
+  $menu_contact = $row['menu_contact'];
+  $title_about = $row['title_about'];
 }
-?>
+ ?>
