53f23be9fae58eb67973c9313643b3e4b98b1278
Micke <mikael.johanssonj@gmail.om>
2021-03-23 16:19:14 +0100

    c

--- includes/config.php
+++ includes/config.php
@@ -7,12 +7,13 @@ function OpenCon()
  $dbpass = "";
  $db = "webbservone";
  $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+}
 ?>
 
-<?php {
+<?php
 $query = mysqli_query($conn, "SELECT * FROM tbl_articles");
 while($row = mysqli_fetch_assoc($query))
-
+{
 $menu_about = $row['menu_about'];
 $menu_brand = $row['menu_brand'];
 $menu_specials = $row['menu_specials'];
