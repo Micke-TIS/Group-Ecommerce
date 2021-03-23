b04eda33ec86a457dd13f4b609ff8f039e70668e
Oliwer Löfgren <oliwerl@outlook.com>
2021-03-23 15:55:34 +0100

    menu databas fixad

--- includes/config.php
+++ includes/config.php
@@ -5,6 +5,17 @@ function OpenCon()
  $dbhost = "localhost";
  $dbuser = "root";
  $dbpass = "";
- $db = "WebbservOne";
+ $db = "webbservone";
  $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+?>
+
+<?php
+$query = mysqli_query($conn, "SELECT * FROM tbl_articles");
+while($row = mysqli_fetch_assoc($query))
+{
+$menu_about = $row['menu_about'];
+$menu_brand = $row['menu_brand'];
+$menu_specials = $row['menu_specials'];
+$menu_contact = $row['menu_contact'];
+}
 ?>
