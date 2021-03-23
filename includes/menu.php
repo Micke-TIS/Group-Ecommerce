50046430677e90ed51683a323ee556f7ebb5b9b4
Micke <mikael.johanssonj@gmail.om>
2021-03-23 17:01:00 +0100

    c

--- includes/menu.php
+++ includes/menu.php
@@ -25,7 +25,7 @@
                         <div class="limit-box">
                             <nav class="main-menu">
                                 <ul class="menu-area-main">
-                                  //Jag ändrade alla.html till .php. 
+                                  <!--Jag ändrade alla.html till .php. -->
                                     <li class="active"> <a href="index.html">Home</a> </li>
                                     <li> <a href="about.php"><?php echo $menu_about; ?></a> </li>
                                     <li><a href="brand.php"><?php echo $menu_brand; ?></a></li>
