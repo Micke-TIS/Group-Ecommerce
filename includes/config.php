7f6f80b469109cda6ff1adb6713d9006c50d8e70
admin <46692365+deluxe0x10@users.noreply.github.com>
2021-03-24 10:20:31 +0100

    Few security improvements.

--- includes/config.php
+++ includes/config.php
@@ -1,19 +1,26 @@
 <?php
 //Database connection
 
-$servername='localhost';
+$host='127.0.0.1'; // Changed because this is faster, localhost needs a dns lookup meanwhile this doesn't
 $username='root';
 $password='';
 $dbname = "webbservone";
-$conn=mysqli_connect($servername,$username,$password,"$dbname");
-if(!$conn){
-   die('Could not Connect My Sql:' .mysql_error());
+
+$options = [
+  PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+  PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+  $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password, $options);
+} catch (\PDOException $e) {
+  throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
 
-
 //Select specific columns from DB
-$query = mysqli_query($conn, "SELECT * FROM tbl_articles");
-while($row = mysqli_fetch_assoc($query))
+$stmt = $pdo->query('SELECT * FROM tbl_articles LIMIT 1');
+while ($row = $stmt->fetch())
 {
   $menu_about = $row['menu_about'];
   $menu_brand = $row['menu_brand'];
@@ -21,4 +28,5 @@ while($row = mysqli_fetch_assoc($query))
   $menu_contact = $row['menu_contact'];
   $title_about = $row['title_about'];
 }
- ?>
+
+?>
