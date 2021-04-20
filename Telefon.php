97dffcace53d107f4951ca88c6171f3028f6ae11
Don leal <tismma18krilea@innovationsgymnaiset.se>
2021-04-20 15:36:26 +0200

    Fixade en ny sida där vi skriver fakta om mobilen

---
+++ Telefon.php
@@ -0,0 +1,45 @@
+<?php
+// Some includes for header (Kristoffer)
+  include_once("includes/header.php");
+  include_once("includes/config.php");
+?>
+<!-- body -->
+
+
+<body class="main-layout">
+
+  <?php
+  // Some includes for loader(Kristoffer)
+    include_once("includes/loader.php");
+  ?>
+
+
+
+
+
+  <?php
+  // Some includes for menu (Aylin)
+    include_once("includes/menu.php");
+  ?>
+
+    <div class="brand_color">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="titlepage">
+                        <h2><?php echo $title_about; ?></h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php
+    // Some includes for about (oliwer)
+      include_once("includes/about.php");
+    ?>
+
+    <?php
+    // Some includes for footer (Juliet)
+      include_once("includes/footer.php");
+    ?>
