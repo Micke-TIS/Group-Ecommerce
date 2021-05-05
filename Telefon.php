b0be53fe24e77daff721a25861977c4342ba2157
donleal <tismma18krilea@innovationsgymnasiet.se>
2021-05-05 10:31:45 +0200

    Fixa text till telefon

--- Telefon.php
+++ Telefon.php
@@ -34,9 +34,30 @@
         </div>
     </div>
 
-    <?php
-    // Some includes for about (oliwer)
-      include_once("includes/about.php");
+    <!-- about -->
+    <div class="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-5 col-lg-5 col-md-5 co-sm-l2">
+                    <div class="about_img">
+                        <figure><img src="images/about.png" alt="img" /></figure>
+                    </div>
+                </div>
+                <div class="col-xl-7 col-lg-7 col-md-7 co-sm-l2">
+                    <div class="about_box">
+                        <h3>Intoducing TIS Phone 18</h3>
+                        <span>Mycket mer än mycket snabbare.</span>
+                        <p>
+5G blir proffs. A14 Bionic dundrar förbi alla andra smartphone‑chip. Proffskamerasystemet tar fotogra­fering i svagt ljus till nästa nivå – klivet är ännu större för iPhone 12 Pro Max. Och Ceramic Shield ger fyra gånger bättre skydd om du tappar din iPhone i marken. Låt oss ta en närmare titt på vad den kan göra.</p>
+
+                    </div>
+
+            </div>
+        </div>
+    </div>
+    </div>
+    <!-- end about -->
+
     ?>
 
     <?php
