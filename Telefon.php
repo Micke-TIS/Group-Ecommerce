dd6abac556d4d791899f2e6bd8fb50420a4a915f
donleal <tismma18krilea@innovationsgymnasiet.se>
2021-05-05 10:36:14 +0200

    bild

--- Telefon.php
+++ Telefon.php
@@ -40,7 +40,7 @@
             <div class="row">
                 <div class="col-xl-5 col-lg-5 col-md-5 co-sm-l2">
                     <div class="about_img">
-                        <figure><img src="images/about.png" alt="img" /></figure>
+                        <figure><img src="images/3.png" alt="img" /></figure>
                     </div>
                 </div>
                 <div class="col-xl-7 col-lg-7 col-md-7 co-sm-l2">
