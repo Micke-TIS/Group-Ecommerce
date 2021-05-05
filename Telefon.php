b9fdcddf1998aa2e2ed34f5496c8d607dbabb37d
donleal <tismma18krilea@innovationsgymnasiet.se>
2021-05-05 10:41:44 +0200

    .

--- Telefon.php
+++ Telefon.php
@@ -45,10 +45,10 @@
                 </div>
                 <div class="col-xl-7 col-lg-7 col-md-7 co-sm-l2">
                     <div class="about_box">
-                        <h3>TIS Phone 18</h3>
+                        <h3>TIS Phone 18 och TIS Phone 18 pro</h3>
                         <span>Mycket mer än mycket snabbare.</span>
                         <p>
-5G blir proffs. A14 Bionic dundrar förbi alla andra smartphone‑chip. Proffskamerasystemet tar fotogra­fering i svagt ljus till nästa nivå – klivet är ännu större för iPhone 12 Pro Max. Och Ceramic Shield ger fyra gånger bättre skydd om du tappar din iPhone i marken. Låt oss ta en närmare titt på vad den kan göra.</p>
+5G blir proffs. A14 Bionic dundrar förbi alla andra smartphone‑chip. Proffskamerasystemet tar fotogra­fering i svagt ljus till nästa nivå – klivet är ännu större för TIS Phone 18. Och Ceramic Shield ger fyra gånger bättre skydd om du tappar din iPhone i marken. Låt oss ta en närmare titt på vad den kan göra.</p>
 
                     </div>
 
