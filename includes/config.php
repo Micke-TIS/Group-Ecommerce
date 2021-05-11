e61718314f6d5164f6f7a29e15fb59b5c6827f48
deluxe0x10 <46692365+deluxe0x10@users.noreply.github.com>
2021-05-11 15:41:11 +0200

    More comments

--- includes/config.php
+++ includes/config.php
@@ -6,6 +6,7 @@ $username='root';
 $password='';
 $dbname = "webbservone";
 
+// PDO Options, how errors are handled etc.   
 $options = [
   PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
   PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
@@ -13,11 +14,13 @@ $options = [
 ];
 
 try {
+  // Setting up the PDO object.
   $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password, $options);
 } catch (\PDOException $e) {
   throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
 
+// Check if tbl_products table doesn't exist, if not we create it.
 $pdo->query("CREATE TABLE IF NOT EXISTS `tbl_products` (
   `product_id` INT AUTO_INCREMENT NOT NULL,
   `name` varchar(150) NOT NULL,
@@ -27,7 +30,7 @@ $pdo->query("CREATE TABLE IF NOT EXISTS `tbl_products` (
   CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci")->execute();
 
 //Select specific columns from DB
-$stmt = $pdo->query('SELECT * FROM tbl_articles LIMIT 1');
+$stmt = $pdo->query('SELECT menu_about, menu_brand, menu_specials, menu_contact, menu_telefon, title_about FROM tbl_articles LIMIT 1'); // Select the columns we need(a bit faster)
 while ($row = $stmt->fetch())
 {
   //Meny kopplad till databasen gjord av Oliwer
