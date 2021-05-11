9e53180eaa3dd86dd94e1510cf5bd615e332a166
donleal <tismma18krilea@innovationsgymnasiet.se>
2021-05-11 15:26:40 +0200

    kommentera och dynamsik

--- Telefon.php
+++ Telefon.php
@@ -34,29 +34,11 @@
         </div>
     </div>
 
-    <!-- telefon fakta -->
-    <div class="about">
-        <div class="container">
-            <div class="row">
-                <div class="col-xl-5 col-lg-5 col-md-5 co-sm-l2">
-                    <div class="about_img">
-                        <figure><img src="images/5.png" alt="img" /></figure>
-                    </div>
-                </div>
-                <div class="col-xl-7 col-lg-7 col-md-7 co-sm-l2">
-                    <div class="about_box">
-                        <h3>TIS Phone 18 och TIS Phone 18 pro</h3>
-                        <span>Mycket mer än mycket snabbare.</span>
-                        <p>
-5G blir proffs. A14 Bionic dundrar förbi alla andra smartphone‑chip. Proffskamerasystemet tar fotogra­fering i svagt ljus till nästa nivå – klivet är ännu större för TIS Phone 18. Och Ceramic Shield ger fyra gånger bättre skydd om du tappar din iPhone i marken. Låt oss ta en närmare titt på vad den kan göra.</p>
-
-                    </div>
-
-            </div>
-        </div>
-    </div>
-    </div>
-    <!-- end telefon fakta -->
+    <?php
+    // Some includes for telefon(Kristoffer)
+    //  //Gjorde så att texten som finns i includes/telefon.php är inkluderad i telefon.php som är vad som syns på hemsidan. Alltså istället för att all kod är på denna sida gör man koden blir dynamisk så att det blir enklare att kopia ifall men behöver. Detta kan vara bra ifall man ska kopiera headern eller footern till olika sidor.
+      include_once("includes/telefon.php");
+    ?>
 
     ?>
 
